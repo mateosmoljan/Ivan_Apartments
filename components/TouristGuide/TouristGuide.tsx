149d0dfa7416f4df7221e1666b48b43ff1c17249
SudoDosu <93739034+SudodoSu@users.noreply.github.com>
2024-07-19 22:06:16 +0200

    style

--- components/TouristGuide/TouristGuide.tsx
+++ components/TouristGuide/TouristGuide.tsx
@@ -12,10 +12,10 @@ function TouristGuide() {
         <div className="container">
           <div className="w-6xl flex flex-col gap-3 pb-10">
             <h2 className="text-pink font-semibold uppercase tracking-widest">
-              {PhotogalleriesData.data[0].subtitle}
+              {PhotogalleriesData.images[0].subtitle}
             </h2>
             <h1 className="font-bold text-4xl sm:text-3xl text-dark_blue_black">
-              {PhotogalleriesData.data[0].title}
+              {PhotogalleriesData.images[0].title}
             </h1>
           </div>
           <TouristSwiper />
